628a877ef06ba7507dd3ebaac901e27e06e5cf12
robski577 <robski577@gmail.com>
2016-11-18 16:47:16 -0600

    Initial commit

---
+++ personal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me','robski577@gmail.com']})
+
+
+# Create your views here.
